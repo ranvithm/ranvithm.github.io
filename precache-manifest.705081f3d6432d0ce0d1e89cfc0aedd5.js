5dc5cb13b2ed6a6f654a23c3f87be6e725bd9582
Ranjithkumar-M <ranvitranjit@gmail.com>
2021-08-26 11:32:33 +0530

    Updated version

---
+++ precache-manifest.705081f3d6432d0ce0d1e89cfc0aedd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db8bca530eec4ea1b8e3d642198b359",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18be0cf8e7078ebaf462",
+    "url": "/static/css/main.f8f9c7b8.chunk.css"
+  },
+  {
+    "revision": "8d3cd37592258eaea440",
+    "url": "/static/js/2.0f6d47ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f6d47ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18be0cf8e7078ebaf462",
+    "url": "/static/js/main.67794ec8.chunk.js"
+  },
+  {
+    "revision": "84ac65994d53f35e12bc",
+    "url": "/static/js/runtime-main.82de50f5.js"
+  }
+]);
